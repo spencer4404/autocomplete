824b24434785ea585d24687065007e4aac41b302
Spencer Iannantuono <133061786+spencer4404@users.noreply.github.com>
2025-02-11 21:34:57 -0500

    finishing build_tree, added some checks

--- autocomplete.py
+++ autocomplete.py
@@ -6,21 +6,51 @@ import string
 
 class Node:
     #TODO
-    def __init__(self):
+    def __init__(self, letter = ""):
         self.children = {}
-        # self.is_word = False
+        self.letter = letter
+        self.is_word = False
+
+    def __str__(self):
+        return f"{self.letter}: Node({self.letter})"
 
 class Autocomplete():
     def __init__(self, parent=None, document=""):
         self.root = Node()
         self.suggest = self.suggest_random #Default, change this to `suggest_dfs/ucs/bfs` based on which one you wish to use.
+        # for checking purposes
+        self.all_words = []
 
+    # build the tree
     def build_tree(self, document):
         for word in document.split():
             node = self.root
             for char in word:
                 #TODO for students
-                pass
+                # If the node for the letter does not exist under the root, create it
+                if char not in list(node.children.keys()):
+                    node.children[char] = Node(char)
+                # jump down to new node
+                node = node.children[char]
+            # mark last node as the end of the word
+            node.is_word = True
+
+    # recursive function to see the tree, basically a DFS
+    def print_tree(self, node=None, prefix=""):
+        # base case
+        if node == None:
+            node = self.root
+        # end
+        if node.is_word:
+            print(f"Word: {prefix}")
+            self.all_words.append(prefix) # for checking purposes
+        # recursive call
+        for char, child in node.children.items():
+            self.print_tree(child, prefix + char)
+
+    # checking
+    def check_tree(self, tree_words = list, words = list):
+        return sorted(tree_words) == sorted(words)
 
     def suggest_random(self, prefix):
         random_suffixes = [''.join(random.choice(string.ascii_lowercase) for _ in range(3)) for _ in range(5)]
@@ -30,13 +60,19 @@ class Autocomplete():
     def suggest_bfs(self, prefix):
         pass
 
-    
-
     #TODO for students!!!
     def suggest_dfs(self, prefix):
         pass
 
-
     #TODO for students!!!
     def suggest_ucs(self, prefix):
         pass
+
+
+a = Autocomplete()
+words = "lit liter no cap bet fam fire tbh fr extra salty shook lowkey highkey vibe check sus simp ghosting salty snatched outfit cancelled shook tea is sis bruh bestie receipts facts curve basic extra totally  af simping cancelled glowed up mood flex clout drip fire iconic slay queen woke fam goals snatched tea no  savage shook lowkey highkey cap vibe check sus simp salty snatched cancelled shook tea sis bruh bestie receipts facts curve basic extra af glowed up mood flex clout drip iconic slay queen woke fam goals snatched tea savage periodt no cap finna turnt snatched tea savage shook lowkey vibe check sus simp salty snatched cancelled shook sis bruh bestie receipts facts curve basic extra af simping cancelled glowed up mood flex clout drip iconic slay queen woke goals tea savage lit no cap bet fam fire tbh fr extra salty shook lowkey vibe check sus simp ghosting salty snatched cancelled shook tea sis bruh bestie receipts facts curve basic extra af simping cancelled glowed up mood flex clout drip iconic slay queen woke fam goals snatched tea savage snatched receipts vibe check salty ghosting mood clout glow up facts sus fam basic slay there though that the their through thee thou thought thag"
+a.build_tree(words)
+a.print_tree()
+print(" ".join(sorted(a.all_words)))
+print(" ".join(sorted(set(words.split()))))
+print(a.check_tree(a.all_words, set(words.split())))
--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from autocomplete import Autocomplete
 from utilities import read_file, create_gui
 
 autocomplete_engine = Autocomplete()
-filename = 'genZ.txt'
+filename = 'test.txt'
 read_file(filename, autocomplete_engine)
 create_gui(autocomplete_engine)
